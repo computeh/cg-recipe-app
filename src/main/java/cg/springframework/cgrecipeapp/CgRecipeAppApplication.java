8b5a3c48f3134246072f9c592630e37be6ae0560
Caleb Ghirmai <calebyg15@gmail.com>
2021-02-02 18:46:05 -0800

    Used Spring Initialzr to create project with maven dependencies.

---
+++ src/main/java/cg/springframework/cgrecipeapp/CgRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package cg.springframework.cgrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cg/springframework/cgrecipeapp/CgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.springframework.cgrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
