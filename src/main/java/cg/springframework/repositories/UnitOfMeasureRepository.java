372e50ca0e5bfcc9ae352bd52e32169ea1438799
Caleb Ghirmai <calebyg15@gmail.com>
2021-02-14 20:07:19 -0800

    Added Repository interfaces.

---
+++ src/main/java/cg/springframework/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package cg.springframework.repositories;
+
+import cg.springframework.model.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
